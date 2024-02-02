91a1cf2d358f8459ac00261cbf2e771c5b495cba
theangkko <theangkko@gmail.com>
2024-02-02 01:28:43 +0000

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://theangkko.github.io',
-	base: '/eefunction-doc',
+	base: '/',
 	integrations: [
 		starlight({
 			title: 'My Docs',
