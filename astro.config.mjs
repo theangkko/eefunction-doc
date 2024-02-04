1db1dc9397162864e9ea4a144ac8c905b54d0965
theangkko <theangkko@gmail.com>
2024-02-04 12:21:26 +0000

    link change

--- astro.config.mjs
+++ astro.config.mjs
@@ -29,8 +29,8 @@ export default defineConfig({
 					label: 'Functions',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'General', link: '/functions/general/' },
-						{ label: 'Engineering', link: '/functions/engineering/' },
+						{ label: 'General Function', link: '/functions/general-function/' },
+						{ label: 'Engineering Function', link: '/functions/engineering-function/' },
 					],
 				},
 				{
